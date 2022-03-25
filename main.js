d437a2ade95c23deb8d324ca9ef4b4dee727d5c1
Cristhian Morales <camoralesh27@gmail.com>
2022-03-24 20:13:12 -0700

    nuestro primer proyecto

---
+++ main.js
@@ -0,0 +1,55 @@
+//foco 1
+function turnOn1(){
+    document.getElementById("foco1").src="./img/turnOn.png";
+}
+function turnOff1(){
+    document.getElementById("foco1").src="./img/turnOff.png";
+}
+function verde1(){
+    document.getElementById("foco1").src="./img/green.png";
+}
+function rojo1(){
+    document.getElementById("foco1").src="./img/red.png";
+}
+
+//foco2
+function turnOn2(){
+    document.getElementById("foco2").src="./img/turnOn.png";
+}
+function turnOff2(){
+    document.getElementById("foco2").src="./img/turnOff.png";
+}
+function verde2(){
+    document.getElementById("foco2").src="./img/green.png";
+}
+function rojo2(){
+    document.getElementById("foco2").src="./img/red.png";
+}
+
+//foco3
+function turnOn3(){
+    document.getElementById("foco3").src="./img/turnOn.png";
+}
+function turnOff3(){
+    document.getElementById("foco3").src="./img/turnOff.png";
+}
+function verde3(){
+    document.getElementById("foco3").src="./img/green.png";
+}
+function rojo3(){
+    document.getElementById("foco3").src="./img/red.png";
+}
+
+//foco4
+function turnOn4(){
+    document.getElementById("foco4").src="./img/turnOn.png";
+}
+function turnOff4(){
+    document.getElementById("foco4").src="./img/turnOff.png";
+}
+function verde4(){
+    document.getElementById("foco4").src="./img/green.png";
+}
+function rojo4(){
+    document.getElementById("foco4").src="./img/red.png";
+}
